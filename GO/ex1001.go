ecd9d0a33c10617fe0bb3381e62c503eff3e7e30
EderCSantana <eder.cardoso.santana@gmail.com>
2022-06-30 14:19:00 +0200

    go

--- GO/ex1001.go
+++ GO/ex1001.go
@@ -1,26 +1,32 @@
 /*
 package main
- 
+
 import (
     "fmt"
 )
- 
+
 func main() {
     var a,b int
     var x int
- 
+
     fmt.Scanf("%d", &a)
     fmt.Scanf("%d", &b)
     x = a + b
-     
+
     fmt.Printf("X = %d\n", x)
 }
 */
 package main
 
-import(
+import (
 	"fmt"
 )
-func main(){
-	const NumA = 
+
+func main() {
+	var NumA, NumB, Sum int
+	fmt.Scanf("%d", &NumA)
+	fmt.Scanf("%d", &NumB)
+
+	Som = NumA + NumB
+	fmt.Printf("X = %d", Sum)
 }

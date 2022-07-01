12d16a27ff0c1e52bb3a7c42112c3707ecb88dda
EderCSantana <eder.cardoso.santana@gmail.com>
2022-07-01 21:32:38 +0200

    more set ups

--- Javascript/ex0001.js
+++ Javascript/ex0001.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+var input = require('fs').readFileSync('Javascript/stdin', 'utf8');
+var lines = input.split('\n');
+/*format of iuput*/
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+console.log('X = ' + (a + b));
---
+++ Javascript/ex1001/ex1001.js
@@ -0,0 +1,19 @@
+/*format of iuput
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+    
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+   
+console.log('X = ' + (a+b));
+
+
+*/
+var input = require('fs').readFileSync('Javascript/ex1001/olar', 'utf8');
+var lines = input.split('\n');
+/*format of iuput*/
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+console.log('X = ' + (a + b));

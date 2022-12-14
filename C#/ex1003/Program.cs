ce536236711299e91a1e8664e7221f10202e893b
EderCSantana <eder.cardoso.santana@gmail.com>
2022-12-14 09:52:19 +0100

    C++ e C# 1003

---
+++ C#/ex1003/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication1003{
+    class program1003{
+        static void Main(string[] args){
+            int a, b, soma;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            soma = a + b;
+            Console.WriteLine("SOMA = {0}", soma);
+
+        }
+    }
+}
---
+++ C++/ex1003.c++
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main (){
+    int a, b, soma;
+    cin >> a;
+    cin >> b;
+    soma = a + b;
+    cout << "SOMA = " << soma << endl;
+}

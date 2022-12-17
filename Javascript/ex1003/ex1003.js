2ed79d146dea807768fea3579fbb0e632ddcc8d5
EderCSantana <eder.cardoso.santana@gmail.com>
2022-12-17 20:45:41 +0100

    js 1003

---
+++ Javascript/ex1003/ex1003.js
@@ -0,0 +1,17 @@
+/*
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+
+var input = require("fs").readFileSync('Javascript/ex1002/stdin', 'utf8');
+var radius = parseFloat(input);
+var Pi = 3.14159;
+var area = Math.pow(radius, 2) * Pi;
+console.log('A=' + area.toFixed(4));
+*/
+
+var input = require("fs").readFileSync('Javascript/ex1003/stdin', 'utf8');
+var
+var a = parseInt(input);
+var b = parseInt(input);
+var soma = a + b;
+
+console.log('SOMA = ' SOMA);

d401903acf99294a54218b2dfbc08ceb326c6ee8
EderCSantana <eder.cardoso.santana@gmail.com>
2023-01-23 21:53:23 +0100

    C# and python 1006

---
+++ Python/ex1006.py
@@ -0,0 +1,5 @@
+a = float(input("Write a number"))
+b = float(input("Write a number"))
+c = float(input("Write a number"))
+media = (a * 2 + b * 3 + c * 5)/10
+print("MEDIA = {:.1f}".format(media), end="")

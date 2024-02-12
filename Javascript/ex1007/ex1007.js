cf266a21b27b5f168e432e4bb0394c357a98afa2
EderCSantana <eder.cardoso.santana@gmail.com>
2024-02-12 17:01:14 +0100

    1

--- Javascript/ex1007/ex1007.js
+++ Javascript/ex1007/ex1007.js
@@ -1,13 +1,16 @@
 // var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 // var lines = input.split('\n');
 
-var input = require("fs").readFileSync("Javascript/ex1007/stdin", 'utf8');
+var input = require("fs").readFileSync("./stdin", 'utf8');
 var valores = input.split("\n");
 
 var A = parseInt(valores.shift());
 var B = parseInt(valores.shift());
 var C = parseInt(valores.shift());
 var D = parseInt(valores.shift());
-var dif = (A * B - C * D);
 
-console.log("DIFERENCA = " + dif);
+export function diferenca(A, B, C, D) {
+	return (A * B - C * D);
+}
+
+console.log("DIFERENCA = " + diferenca(A, B, C, D));

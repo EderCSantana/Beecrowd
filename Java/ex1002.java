ae5445a360fe049355b1ad06f4fee4d014eb0681
EderCSantana <eder.cardoso.santana@gmail.com>
2024-04-03 18:59:00 +0200

    1

--- Java/ex1002.java
+++ Java/ex1002.java
@@ -5,6 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader ir = new InputStreamReader(System.in);
-		
+		BufferedReader in = new BufferedReader(ir);
+
+		float R, A;
+		double n = 3.14159;
+
+		R = Float.parseFloat(in.readLine());
+		A = n * R * R;
+
+		System.out.printf("A=%.4f\n", A);
 	}
 }

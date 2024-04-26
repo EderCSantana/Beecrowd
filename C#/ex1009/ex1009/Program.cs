b2388b865b59059ee277f2b211e7046d4e0e8967
EderCSantana <eder.cardoso.santana@gmail.com>
2024-04-26 18:45:36 +0200

    1

--- C#/ex1009/ex1009/Program.cs
+++ C#/ex1009/ex1009/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ex1009{
+	class program1009{
+		static void Main(string[] args){
+			string	name;
+			double	salary, bonus;
+
+			name = Console.ReadLine();
+			salary = double.Parse(Console.ReadLine() ?? "0");
+			bonus = double.Parse(Console.ReadLine() ?? "0");
+			salary = salary + bonus * 0.15;
+			Console.WriteLine("TOTAL = R$ {0:0.00}", salary);
+		}
+	}
+}

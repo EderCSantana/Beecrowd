787a7419248ca322aad9b20d1ae6c4f7015e84a4
EderCSantana <eder.cardoso.santana@gmail.com>
2024-04-29 01:29:09 +0200

    1

---
+++ R/ex1009.r
@@ -0,0 +1,4 @@
+name <- readline(prompt = "")
+salary <- as.double(readline(prompt = ""))
+bonus <- as.double(readline(prompt = ""))
+print(paste0("TOTAL = R$ ",  formatC(total, digits = 2, format = "f"), "\n"))

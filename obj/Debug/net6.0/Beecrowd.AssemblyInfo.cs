f08684524419c53e52380f1151e0b50d5d5abcd6
EderCSantana <eder.cardoso.santana@gmail.com>
2024-05-26 20:09:52 +0200

    1

--- obj/Debug/net6.0/Beecrowd.AssemblyInfo.cs
+++ obj/Debug/net6.0/Beecrowd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Beecrowd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc38b7fb81e1545052c9463e0dbda7e0021c9517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33ac957a23ad3f492f4ac0d53201e82c537053e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Beecrowd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Beecrowd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
